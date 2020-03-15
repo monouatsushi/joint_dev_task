2ba2e9aef486f1221df4fb7e9e6cc16b2cdb37c0
桃生篤 <atumonou883@gmail.com>
2020-03-15 16:32:03 +0900

    Q7を修正、Q8~10に回答

--- task.rb
+++ task.rb
@@ -57,14 +57,15 @@ def q7
   array = ["1", "2", "3", "4", "5"]
 
   # 以下に回答を記載
-  p array.map!{|x| x.to_i}
-
+  p array.map!(&:to_i)
 end
 
 def q8
   programming_languages = %w(ruby php python javascript)
 
   # 以下に回答を記載
+  programming_languages.map!(&:capitalize)
+  upper_case_programming_languages = programming_languages.map(&:upcase)
 
   # 以下は変更しないで下さい
   p programming_languages
@@ -75,6 +76,9 @@ def q9
   names = ["田中", "佐藤", "佐々木", "高橋"]
 
   # 以下に回答を記載
+  names.each.with_index(1) do |name, i|
+    puts "会員No.#{i} #{name}さん"
+  end
 
 end
 
@@ -82,6 +86,11 @@ def q10
   foods = %w(いか たこ うに しゃけ うにぎり うに軍艦 うに丼)
 
   # 以下に回答を記載
+  if foods.include?("うに") 
+    puts "好物です"
+  else
+    p "まあまあ好きです"
+  end
 
 end
 
